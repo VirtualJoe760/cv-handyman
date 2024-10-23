5a7c9ea8850f53a5c7d9bb7e8f49ecbe3356fce2
Joey Sardella <josephsardella@gmail.com>
2024-10-23 11:16:54 -0700

    added og image & favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default async function RootLayout({
             'Coachella Valley Handyman offers expert services for your home improvement needs. Contact us today to schedule an appointment.'
           }
         />
-        <meta property="og:image" content={seoData?.ogImage || '/path-to-your-default-og-image.jpg'} />
+        <meta property="og:image" content={seoData?.ogImage} />
         <meta property="og:type" content="website" />
         <meta property="og:url" content={seoData?.ogUrl || 'https://coachellavalleyhandyman.com'} />
         <meta name="robots" content="index, follow" />
@@ -60,8 +60,7 @@ async function fetchSeoData() {
       'We specialize in a range of home improvement services, including repairs, maintenance, and remodeling. Contact us for fast, reliable service in the Coachella Valley.',
     ogTitle: 'Coachella Valley Handyman',
     ogDescription: 'Get expert handyman services in the Coachella Valley area today!',
-    ogImage: 'https://res.cloudinary.com/dcrue4vr6/image/upload/blog-post/v1729459742/coachella-valley.jpg',
+    ogImage: 'https://res.cloudinary.com/dcrue4vr6/image/upload/l_text:Arial_40_bold:Coachella-Valley-Handyman,co_rgb:ffffff,g_south_east,x_20,y_80,b_rgb:000000/l_text:Arial_50_bold:No task to big or small,co_rgb:ffffff,g_south_east,x_20,y_20,b_rgb:00CC00/v1729459742/coachella-valley.jpg',
     ogUrl: 'https://coachellavalleyhandyman.com',
-    favicon: 'public/favicon.svg'
   };
 }
